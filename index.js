72aaaa1481614a8be3298ff915310bd55e422968
Saint-Jean Germila <germila@noukod.com>
2019-03-06 17:28:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // Code your solutions in this file
-for (let i= 0; i > 5; i++){
-  console.lob (${i});
+function printBages (names)
+{
+  for (let i = 0; i < names; i++){
+  console.log (`Welcome${names[i]}! You are employee #${[i + 1]}.`);
+}
+returns names;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
 }
